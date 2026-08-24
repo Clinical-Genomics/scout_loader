568d4c70d947d73f2b8a6a596db61cf3e4be5466
Chiara Rasi <rasi.chiara@gmacil.com>
2026-08-24 14:17:55 +0200

    Removed debug message

--- src/parse/vcf.rs
+++ src/parse/vcf.rs
@@ -434,7 +434,6 @@ pub async fn process_vcf(
         }
     }
     if !batch.is_empty() {
-        println!("Loading final batch of {} variants", batch.len());
         loader.load_variant_bulk(batch).await?;
     }
 
