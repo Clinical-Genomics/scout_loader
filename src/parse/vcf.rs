b548d5442fc01fd20ecea9fb2f8d9aa1bbe28449
Chiara Rasi <rasi.chiara@gmacil.com>
2026-08-24 08:11:35 +0200

    Remove empty line

--- src/parse/vcf.rs
+++ src/parse/vcf.rs
@@ -157,7 +157,6 @@ pub fn add_hgnc_symbols(variant: &mut Document, hgncid_to_gene: &HashMap<i32, Do
 ///
 /// Panics if the VCF file cannot be opened, if the sample mapping cannot be
 /// created, or if a record cannot be read.
-
 pub async fn process_vcf(
     path: &str,
     category: VariantCategory,
