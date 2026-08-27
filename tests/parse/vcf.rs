60cd29ae924666f15e25a39a5c8668224f207a50
Chiara Rasi <rasi.chiara@gmacil.com>
2026-08-27 11:21:03 +0200

    Attempt at fixing test

--- tests/parse/vcf.rs
+++ tests/parse/vcf.rs
@@ -2,10 +2,13 @@ use mongodb::bson::doc;
 
 use scout_loader::models::variant::VariantCategory;
 use scout_loader::parse::vcf::should_load_variant;
+use scout_loader::utils::hash::generate_md5_key;
+use std::collections::HashSet;
 
 #[test]
 fn test_should_load_variant() {
     let threshold = 5;
+    let managed_variant_ids = HashSet::new();
 
     let cases = [
         // No rank score.
@@ -78,6 +81,37 @@ fn test_should_load_variant() {
     ];
 
     for (variant, category, expected) in cases {
-        assert_eq!(should_load_variant(&variant, category, threshold), expected);
+        assert_eq!(
+            should_load_variant(&variant, category, threshold, &managed_variant_ids),
+            expected,
+        );
     }
+}
+
+#[test]
+fn should_load_managed_variant() {
+    let variant = doc! {
+        "chromosome": "5",
+        "position": 112043220,
+        "reference": "A",
+        "alternative": "C",
+        "rank_score": 0,
+    };
+
+    let managed_variant_id = generate_md5_key(&[
+        "5".to_string(),
+        "112043220".to_string(),
+        "A".to_string(),
+        "C".to_string(),
+        "clinical".to_string(),
+    ]);
+
+    let managed_variant_ids = HashSet::from([managed_variant_id]);
+
+    assert!(should_load_variant(
+        &variant,
+        VariantCategory::Snv,
+        5,
+        &managed_variant_ids,
+    ));
 }
